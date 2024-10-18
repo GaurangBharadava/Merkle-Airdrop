e86714020a98f562f8b8c512702e9e4933d72837
GaurangBharadava <gaurangbrdv1357@gmail.com>
2024-10-19 01:06:40 +0530

    second commit

--- src/MerkleAirdrop.sol
+++ src/MerkleAirdrop.sol
@@ -5,10 +5,10 @@ import {IERC20, SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeE
 import {MerkleProof} from "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
 
 contract MerkleAirdrop {
-
     using SafeERC20 for IERC20;
     //List of address.
     //Allow aomeone in the list to claim token.
+
     error MerkleAirdeop__InvalidProof();
     error MerkleAirdeop__HasClaimed();
 
@@ -25,29 +25,27 @@ contract MerkleAirdrop {
     }
 
     function claim(address account, uint256 amount, bytes32[] calldata merkleProof) external {
-        if(s_hasClaimed[account] == true) {
+        if (s_hasClaimed[account] == true) {
             revert MerkleAirdeop__HasClaimed();
         }
-        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account,amount))));
-        if(!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(account, amount))));
+        if (!MerkleProof.verify(merkleProof, i_merkleRoot, leaf)) {
             revert MerkleAirdeop__InvalidProof();
         }
 
         s_hasClaimed[account] = true;
-        emit Claim(account,amount);
+        emit Claim(account, amount);
 
         // i_airderopToken.transfer(account,amount);
         //we will use safe Transfer function of SafeERC20 token. that will handles revert and error for us.
-        i_airderopToken.safeTransfer(account,amount);
-        
+        i_airderopToken.safeTransfer(account, amount);
     }
 
-    function getMerkleRoot() external view returns(bytes32) {
+    function getMerkleRoot() external view returns (bytes32) {
         return i_merkleRoot;
     }
 
-    function getAirdropToken() external view returns(IERC20) {
+    function getAirdropToken() external view returns (IERC20) {
         return i_airderopToken;
     }
-
 }
--- test/MerkleAirdropTest.t.sol
+++ test/MerkleAirdropTest.t.sol
@@ -7,10 +7,9 @@ import {MerkleAirdrop} from "../src/MerkleAirdrop.sol";
 import {DeployMerkleAirdrop} from "../script/DeployMerkleAirdrop.s.sol";
 import {ZkSyncChainChecker} from "foundry-devops/src/ZkSyncChainChecker.sol";
 
-contract MerkleAirdropTest is  ZkSyncChainChecker, Test {
-
+contract MerkleAirdropTest is ZkSyncChainChecker, Test {
     MerkleAirdrop public airdrop;
-    GBToken public  token;
+    GBToken public token;
 
     bytes32 public ROOT = 0xaa5d581231e596618465a56aa0f5870ba6e20785fe436d5bfb82b08662ccc7c4;
     address user;
@@ -21,18 +20,18 @@ contract MerkleAirdropTest is  ZkSyncChainChecker, Test {
     bytes32 proofOne = 0x0fd7c981d39bece61f7499702bf59b3114a90e66b51ba2c53abdf7b62986c00a;
     bytes32 proofTwo = 0xe5ebd1e1b5a5478a944ecab36a9a954ac3b6b8216875f6524caa7a1d87096576;
     bytes32[] PROOF = [proofOne, proofTwo];
-    function setUp() public {
 
-        if(!isZkSyncChain()) {
+    function setUp() public {
+        if (!isZkSyncChain()) {
             DeployMerkleAirdrop deployer = new DeployMerkleAirdrop();
             (airdrop, token) = deployer.run();
         } else {
             token = new GBToken();
-            airdrop = new MerkleAirdrop(ROOT,token);
+            airdrop = new MerkleAirdrop(ROOT, token);
             token.mint(token.owner(), AMOUNT_TO_MINT);
-            token.transfer(address(airdrop),AMOUNT_TO_MINT);
+            token.transfer(address(airdrop), AMOUNT_TO_MINT);
         }
-        (user,userPrevKey) = makeAddrAndKey("user");
+        (user, userPrevKey) = makeAddrAndKey("user");
     }
 
     function testUserCanClaim() public {
@@ -44,10 +43,9 @@ contract MerkleAirdropTest is  ZkSyncChainChecker, Test {
 
         uint256 endingUserBalance = token.balanceOf(user);
 
-        console.log("Ending Balance:",endingUserBalance);
-        console.log("Starting Balance:",startingUserBalance);
+        console.log("Ending Balance:", endingUserBalance);
+        console.log("Starting Balance:", startingUserBalance);
 
-        assertEq(endingUserBalance - startingUserBalance,AMOUNT);
-
+        assertEq(endingUserBalance - startingUserBalance, AMOUNT);
     }
 }
